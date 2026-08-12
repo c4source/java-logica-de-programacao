04a55565e340f9ad94d65a30ed66ac876109f413
c4source <gabrielhenriques4@gmail.com>
2026-08-12 09:40:28 -0300

    Exercicios 43 e 44

---
+++ src/passo04RepeticoesWhile/Ex044ContagemIncremental.java
@@ -0,0 +1,49 @@
+    package passo04RepeticoesWhile;
+
+    import java.util.Scanner;
+
+    public class Ex044ContagemIncremental {
+        public static void main(String[] args) {
+
+            /*
+            exercicio 044
+            Crie um algoritmo que leia o valor inicial da contagem, o valor final e o
+            incremento, mostrando em seguida todos os valores no intervalo:
+            Ex: Digite o primeiro Valor: 3
+            Digite o último Valor: 10
+            Digite o incremento: 2
+            Contagem: 3 5 7 9 Acabou!
+             */
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.printf("Digite o primeiro valor: ");
+            int primeiroValor = input.nextInt();
+
+            System.out.printf("Digite o ultimo valor: ");
+            int ultimoValor = input.nextInt();
+
+            System.out.printf("Digite o valor do incremento: ");
+            int valorEncremento = input.nextInt();
+
+            if (primeiroValor > ultimoValor) {
+                System.out.println("Valor inválido");
+            }
+            else  if ( valorEncremento < 0) {
+                System.out.println("Encremento inválido");
+            }
+            else {
+
+                int contador = primeiroValor;
+
+                while (contador <= ultimoValor) {
+                    System.out.printf("%d ", contador);
+                    contador += valorEncremento;
+                }
+
+            }
+            System.out.println("Acabou!");
+
+
+        }
+    }
